e65f7821093eb0c696af3300cb531307393a4e19
Friedrich482 <friedricht109@gmail.com>
2023-12-30 01:31:19 +0100

    Minimal code for the project

---
+++ Simple_form_registration/index.php
@@ -0,0 +1,17 @@
+<?php
+    include("database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    
+</body>
+</html>
