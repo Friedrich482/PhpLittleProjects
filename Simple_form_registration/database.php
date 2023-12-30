14a7b7c5075aa3da0066bef2d16fe0ccfc8c6192
Friedrich482 <friedricht109@gmail.com>
2023-12-30 12:19:42 +0100

    Created the DB and connected it to the index.php file via database.php

---
+++ Simple_form_registration/database.php
@@ -0,0 +1,17 @@
+<?php
+    $dbServer = 'localhost';
+    $dbUser = 'root';
+    $dbPass = '';
+    $dbName = 'user_data';
+    $conn = '';
+
+    try{
+        $conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
+    }
+    catch(mysqli_sql_exception){
+        // echo "Could'nt connect to the db !<br>";
+    }
+    if($conn){
+        // echo "You're connected !<br>";
+    }
+?>
--- Simple_form_registration/index.php
+++ Simple_form_registration/index.php
@@ -12,6 +12,18 @@
     <script src="script.js" defer></script>
 </head>
 <body>
-    
+    <h1>Welcome on my page</h1>
+    <form action="treatment.php" method="post">
+        <label for="email">Enter your email adress : </label><br>
+        <input type="email" name="email" id="email"><br>
+
+        <label for="username">Enter your username :</label><br>
+        <input type="text" name="username" id="username"><br>
+
+        <label for="password">Enter your password :</label><br>
+        <input type="password" name="password" id="password"><br>
+
+        <input type="submit" value="Submit" name="submit">
+    </form>
 </body>
 </html>
